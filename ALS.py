f54a3105e9e87783ccdd5718f897560baab3793a
xinranLiao <xl4703@nyu.edu>
2023-05-05 04:50:07 -0500

    Update

--- ALS.py
+++ ALS.py
@@ -62,6 +62,7 @@ def main(spark, userID):
                     On i.recording_msid = t.recording_msid
         """)
     #interaction_new_id.show()
+    interaction_new_id.createOrReplaceTempView('interaction_new_id')
 
     user_rank = spark.sql("""
                    select user_id,track_new_id, count_pop/count_total as ranking
