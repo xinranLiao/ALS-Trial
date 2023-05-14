edf3152e7163bf4eefb68482a5bd172b8a2606c5
xinranLiao <xl4703@nyu.edu>
2023-05-13 20:17:54 -0400

    Update

--- ALS_compare.py
+++ ALS_compare.py
@@ -25,19 +25,21 @@ import time
 
 def main(spark, userID):
      
-    train = spark.read.parquet(f'hdfs:/user/xl4703_nyu_edu/ALS_train_100.parquet')
+    train = spark.read.parquet(f'hdfs:/user/xl4703_nyu_edu/ALS_train_10.parquet')
     validation = spark.read.parquet(f'hdfs:/user/xl4703_nyu_edu/ALS_validation_without0.parquet')
     #ranking = spark.read.parquet(f'hdfs:/user/xl4703_nyu_edu/user_norm_rank_100.parquet')
     #train.show()
 
     # Build the recommendation model using ALS on the training data
     # Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
-    als = ALS(userCol="user_id", itemCol="track_new_id", ratingCol="normalized_ranking",
+    als = ALS(maxIter=5,
+              regParam = 1.0, rank = 50, alpha = 1.0,
+              userCol="user_id", itemCol="track_new_id", ratingCol="normalized_ranking",
           coldStartStrategy="drop")
     st = time.time()
     model = als.fit(train)
     run_time = time.time() - st
-    print("100% data run-time: " + str(run_time))
+    print("rank = 50, 10% data run-time: " + str(run_time))
 
     '''
     # Prepare the true labels
